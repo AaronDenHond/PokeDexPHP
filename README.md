7f610bfda8291df2d2f226501f656982f4b40a33
Aaron Den Hond <aaron.denhond@gmail.com>
2021-05-17 15:16:35 +0200

    Working form, working fetch, updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # PokeDexPHP
 PokeDex made in PHP
+
+Plan of attack :
+
+1. Make sure everything works (lots of PHP and Apache troubleshooting).
+Check (for now...)
+
+2. Test form, so users can input ID/name of pokemon
+(Check)
+
+3. Fetch Data 
+
+(Check)
+
+4. make variables with important data : pokeName,pokeID,pokeImage,pokeMoves
+   with data from Fetch
+
+(In progress)
--- index.php
+++ index.php
@@ -1,23 +1,61 @@
 <?php
-$world = "Hello World!"; 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-$echo = "Does this work?";
-?>
+//met accolades geen endif, in HTML wel nodig
+
+//fetch poke data
+$pokeRawData = 'https://pokeapi.co/api/v2/pokemon/';
+
+$data = file_get_contents($pokeRawData);
+$pokeData = json_decode($data);
+
+//var dump to check
+/* var_dump($pokeData); */
+
+//variables we need with API data
+$pokeName = $pokeData['name'];
+$pokeID = $pokeData['id'];
+$pokeImage = $pokeData['sprites']['front-default'];
+var_dump($pokeName);
+var_dump($pokeID);
+
+
+
+
+ if (isset($_GET["id"])){
+    $pokemonID = $_GET["id"];   
+    //we slagen pokeID op
+    
+ }
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <p><?php echo $world ?></p>
-    <?php
-     echo "This is also a paragraph!";
-    ?>
-     <p><?php var_dump($_GET);?></p> 
-     <!-- $_GET is superglobal, global scope and automatically declared -->
-     <p><?php var_dump($_GET["aaron"]);?></p>
+
+    <form method="get">
+        Pokemon: <input type="text" name="id"><br>
+      
+        <input type="submit">
+    </form>
+    <br>
+   
+        NAME: <?php echo htmlspecialchars($_GET["id"]); ?>
+        <br>
+         
+   
 </body>
+
+
 </html>
